9434514f8c47fb632a3a79398da90200a289e720
fraol-getu <frraol@gmail.com>
2024-09-03 06:27:41 +0300

    first commit

---
+++ src/home/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image1 from '../asset/251C_Flyer-01.png'
+import Image2 from '../asset/251C_Flyer-02.png'
+import Image3 from '../asset/251C_Flyer-03.png'
+import Image4 from '../asset/251C_Flyer-04.png'
+
+const Home = () => {
+  return (
+    <section className='home-wrapper'>
+    <div className='home-container'>
+    
+     <div className='image-container'>
+     <img src={Image1} alt="image1" className='image' />
+     </div>
+     <div className='image-container'>
+     <img src={Image2} alt="image1" className='image'/>
+     </div>
+     <div className='image-container'>
+     <img src={Image3} alt="image1" className='image'/>
+     </div>
+     <div className='image-container'>
+     <img src={Image4} alt="image1" className='image'/>
+     </div>
+    </div>
+    </section>
+  )
+}
+
+export default Home
